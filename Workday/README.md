6e834b48ad1646763643c8478a5cb71c0925f60b
boen <1997@boenfu.cn>
2020-02-27 17:55:25 +0800

    ++ WorkDay

---
+++ Workday/README.md
@@ -0,0 +1,33 @@
+## WorkDay
+
+#### 每日日报助手
+
+keywords: `powerGlance` `powerItem` `action` `createTask` `getContextIterable`
+
+![效果图](../SCREENSHOT/WorkDay/1.png)
+
+创建后:
+![效果图2](../SCREENSHOT/WorkDay/2.png)
+
+### 使用
+
+#### 1.将应用发布到 makeflow 并安装
+
+#### 2.启动服务
+
+启动当前目录下的 `app.js`
+
+#### 3.给需要监听任务的概览添加此超级概览
+
+![添加 powerGlance ](../SCREENSHOT/WorkDay/3.png)
+
+#### 4.创建或导入一个简单日报流程
+
+[日报流程](./日报.mfp)
+![导入流程](../SCREENSHOT/WorkDay/4.png)
+
+然后配置上应用提供的超级项
+
+![配置超级项](../SCREENSHOT/WorkDay/5.png)
+
+#### 5.创建任务后执行超级项生成日报
--- Workday/app.js
+++ Workday/app.js
@@ -1,75 +1,80 @@
-const { PowerApp } = require("@makeflow/sdk");
-const isToday = require("date-fns/isToday");
-const startOfToday = require("date-fns/startOfToday");
+const {PowerApp} = require('@makeflow/sdk');
+const isToday = require('date-fns/isToday');
+const startOfToday = require('date-fns/startOfToday');
 
 const app = new PowerApp();
 
-app.version("0.1.0", {
+app.version('0.1.0', {
   contributions: {
     powerItems: {
       WorkDay: {
         action: {
-          async create({ inputs }) {
+          async create({inputs}) {
             try {
               let {
                 task,
-                assignee: { id: currentUser, displayName },
-                receiver: { id: receiver },
-                team: { id: team },
-                procedure: { id: procedure }
+                assignee: {id: currentUser, displayName},
+                receiver: {id: receiver},
+                team: {id: team},
+                procedure: {id: procedure},
               } = inputs;
 
-              await app.emitChanges(
-                "power-glance",
+              // 匹配任务负责人今日有完成任务的概览
+              for await (let {storage, api} of app.getContextIterable(
+                'power-glance',
                 {
-                  [currentUser]: startOfToday().getTime()
+                  storage: {
+                    [currentUser]: startOfToday().getTime(),
+                  },
                 },
-                async ({ storage, api }) => {
-                  let tasksDict = storage.get("task") || {};
+              )) {
+                let tasksDict = storage.get('task') || {};
 
-                  let tasks = Object.values(tasksDict).filter(
-                    ({ user, completedAt }) =>
-                      user === currentUser && isToday(completedAt)
-                  );
-                  await api.createTask({
-                    team,
-                    procedure,
-                    brief: `${new Date().toLocaleDateString()} ${displayName} 的每日报表`,
-                    description: `今日完成任务数:${tasks.length}\n${tasks
-                      .map(({ brief, num }) => `#${num} - ${brief}`)
-                      .join("\n")}`,
-                    assignee: receiver,
-                    associatedTasks: [{ type: "blocked-by", task }]
-                  });
-                }
-              );
+                let tasks = Object.values(tasksDict).filter(
+                  ({user, completedAt}) =>
+                    user === currentUser && isToday(completedAt),
+                );
+
+                //创建任务
+                await api.createTask({
+                  team,
+                  procedure,
+                  brief: `${new Date().toLocaleDateString()} ${displayName} 的每日报表`,
+                  description: `今日完成任务数:${tasks.length}\n${tasks
+                    .map(({brief, num}) => `#${num} - ${brief}`)
+                    .join('\n')}`,
+                  assignee: receiver,
+                  associatedTasks: [{type: 'blocked-by', task}],
+                });
+              }
 
               return {
-                stage: "done"
+                stage: 'done',
               };
             } catch (error) {}
-          }
-        }
-      }
+          },
+        },
+      },
     },
     powerGlances: {
       WorkDay: {
-        initialize: handler,
-        change: handler
-      }
-    }
-  }
+        initialize: onGlanceHook,
+        change: onGlanceHook,
+      },
+    },
+  },
 });
 
-async function handler({ resources, storage }) {
+async function onGlanceHook({resources, storage}) {
   let tasksDict = storage.get() || {};
 
   tasksDict.task = tasksDict.task || {};
 
+  // 记录完成的任务的情况
   for (let {
-    ref: { id },
-    inputs: { brief, num, assignee, completedAt },
-    removed
+    ref: {id},
+    inputs: {brief, num, assignee, completedAt},
+    removed,
   } of resources) {
     completedAt = Number(completedAt);
 
@@ -80,7 +85,7 @@ async function handler({ resources, storage }) {
 
     let user = assignee.id;
 
-    tasksDict.task[id] = { brief, num, user, completedAt };
+    tasksDict.task[id] = {brief, num, user, completedAt};
 
     tasksDict[user] = startOfToday().getTime();
   }
@@ -90,6 +95,6 @@ async function handler({ resources, storage }) {
 
 // start
 
-app.koa({
-  port: 9003
+app.serve({
+  port: 9003,
 });
