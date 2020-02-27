bcdb40937c68091018e5d63fbec2138b134ceba9
boen <1997@boenfu.cn>
2020-02-27 17:20:55 +0800

    ++ ReadMe

---
+++ ReadMe/README.md
@@ -0,0 +1,23 @@
+## ReadMe
+
+#### 朕已阅 - 确认文档阅读功能
+
+keywords: `powerItem` `updatePowerItem` `getContextIterable` `action:target`
+
+![效果图](../SCREENSHOT/ReadMe/1.png)
+![效果图2](../SCREENSHOT/ReadMe/2.png)
+
+### 使用
+
+#### 1.将应用发布到 makeflow 并安装
+
+#### 2.启动服务
+
+启动当前目录下的 `app.js`
+
+#### 3.给流程的流程项配置超级项
+
+![添加 powerItem ](../SCREENSHOT/ReadMe/3.png)
+![配置 powerItem ](../SCREENSHOT/ReadMe/4.png)
+
+#### 4.创建对应流程的任务即可使用
--- ReadMe/app.js
+++ ReadMe/app.js
@@ -1,75 +1,71 @@
-const PATH = require("path");
+const PATH = require('path');
 
-const Koa = require("koa");
-const static = require("koa-static");
-const Router = require("koa-router");
+const Koa = require('koa');
+const Static = require('koa-static');
+const Router = require('koa-router');
 
-const { PowerApp } = require("@makeflow/sdk");
+const {PowerApp} = require('@makeflow/sdk');
 
 const app = new Koa();
 
-app.use(static(PATH.join(__dirname, "./static")));
+app.use(Static(PATH.join(__dirname, './static')));
 
 // power app
 
 const powerApp = new PowerApp({
+  // 使用 mongo 做 db
   db: {
-    type: "mongo",
+    type: 'mongo',
     options: {
-      uri: "mongodb://mongo:27017",
-      name: "readme"
-    }
-  }
+      uri: 'mongodb://mongo:27017',
+      name: 'readme',
+    },
+  },
 });
 
-powerApp.version("0.1.0", {
+powerApp.version('0.1.0', {
   contributions: {
     powerItems: {
       readme: {
-        async activate({ inputs, storage }) {
+        // 存储用户ID 和要读的 链接地址
+        async activate({inputs, storage}) {
           await storage.set({
-            [inputs.user.id]: inputs.link
+            [inputs.user.id]: inputs.link,
           });
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 });
 
 // router
 
 let router = new Router();
 
-router.get("/confirm", async ctx => {
-  let { link, user } = ctx.query;
+// 确认阅读页的请求地址
+router.get('/confirm', async ctx => {
+  let {link, user} = ctx.query;
 
   try {
     user = JSON.parse(user).id;
 
-    let params = {
-      [user]: JSON.parse(link)
-    };
+    // 匹配对应用户 ID 和 链接的 power item
+    for await (let {storage, api} of powerApp.getContextIterable('power-item', {
+      storage: {[user]: JSON.parse(link)},
+    })) {
+      await storage.set(user, undefined);
+      // 完成对应超级项
+      await api.updatePowerItem({stage: 'done'});
+    }
 
-    await powerApp.emitChanges(
-      "power-item",
-      params,
-      async ({ storage, api }) => {
-        await storage.set(user, undefined);
-        await api.updatePowerItem({ stage: "done" });
-      }
-    );
-
-    ctx.body = "ok";
+    ctx.body = 'ok';
   } catch (error) {
     ctx.body = error;
   }
 });
 
-app.use(router.routes());
-
-// start
-
-powerApp.koaWith(app, {
-  path: "/mf",
-  port: 9003
-});
+app
+  .use(router.routes())
+  // 设置中间件的 path
+  .use(powerApp.koa('/mf'))
+  .listen(9003);
