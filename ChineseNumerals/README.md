00789eb57a2d0c6469ba5de573e61da442d0d816
boen <1997@boenfu.cn>
2020-02-27 17:26:51 +0800

    ++ ChineseNumerals

---
+++ ChineseNumerals/README.md
@@ -0,0 +1,21 @@
+## ChineseNumerals
+
+#### 财务金额数字转大写
+
+keywords: `powerItem` `outputs`
+
+![效果图](../SCREENSHOT/ChineseNumerals/1.png)
+
+### 使用
+
+#### 1.将应用发布到 makeflow 并安装
+
+#### 2.启动服务
+
+启动当前目录下的 `app.js`
+
+#### 3.给流程的流程项添加超级项
+
+![添加 powerItem ](../SCREENSHOT/ChineseNumerals/2.png)
+
+#### 4.创建对应流程的任务即可使用
--- ChineseNumerals/app.js
+++ ChineseNumerals/app.js
@@ -1,24 +1,26 @@
-const { PowerApp } = require("@makeflow/sdk");
-const toCN = require("./toCN");
+const {PowerApp} = require('@makeflow/sdk');
+// 一段网上找的数字转中文的代码
+const toCN = require('./toCN');
 
 let app = new PowerApp();
 
-app.version("0.1.0", {
+app.version('0.1.0', {
   contributions: {
     powerItems: {
       turn: {
-        update({ inputs }) {
+        update({inputs}) {
           return {
+            // 往任务添加输出
             outputs: {
-              "cn:result": toCN(inputs.number)
-            }
+              'cn:result': toCN(inputs.number),
+            },
           };
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 });
 
 app.serve({
-  port: 9003
+  port: 9003,
 });
