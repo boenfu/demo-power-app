67f56d34efbf160d0b6c860c140b8cb333b0c8a5
boen <1997@boenfu.cn>
2020-02-27 16:50:48 +0800

    ++ BurnDownChart

---
+++ BurnDownChart/README.md
@@ -0,0 +1,23 @@
+## BurnDownChart
+
+#### 迭代燃尽图
+
+keywords: `powerGlance` `reports` `echarts`
+
+![效果图](../SCREENSHOT/BurnDownChart/1.png)
+
+### 使用
+
+#### 1.将应用发布到 makeflow 并安装
+
+#### 2.启动服务
+
+启动当前目录下的 `app.js`
+
+#### 3.给迭代概览添加此应用提供的超级概览
+
+![添加 powerGlance ](../SCREENSHOT/BurnDownChart/2.png)
+
+#### 4.在概览中开启对应图表
+
+![开启 reports](../SCREENSHOT/BurnDownChart/3.png)
--- BurnDownChart/app.js
+++ BurnDownChart/app.js
@@ -1,44 +1,49 @@
-const { PowerApp } = require("@makeflow/sdk");
-const differenceInDays = require("date-fns/differenceInDays");
-const endOfDay = require("date-fns/endOfDay");
+const {PowerApp} = require('@makeflow/sdk');
+const differenceInDays = require('date-fns/differenceInDays');
+const endOfDay = require('date-fns/endOfDay');
 
 const app = new PowerApp();
 
-app.version("0.1.0", {
+app.version('0.1.0', {
   contributions: {
     powerGlances: {
       chart: {
-        initialize: handler,
-        change: handler
-      }
-    }
-  }
+        initialize: onGlanceHook,
+        change: onGlanceHook,
+      },
+    },
+  },
 });
 
-async function handler({ resources, storage, configs }) {
+async function onGlanceHook({resources, storage, configs}) {
   let tasksDict = storage.get() || {};
 
+  // 将概览的任务信息都存储进 storage
   for (let {
-    ref: { id },
-    inputs: { completedAt },
-    removed
+    ref: {id},
+    inputs: {completedAt},
+    removed,
   } of resources) {
     if (removed) {
       delete tasksDict[id];
       continue;
     }
 
+    // 值为 undefined 的属性 在 storage 序列化后会被舍弃
+    // 所以我们用 0 占位
     tasksDict[id] = completedAt || 0;
   }
 
   await storage.set(tasksDict);
 
-  let { days, start } = configs;
+  let {days, start} = configs;
 
-  return getChartData({ days, start, tasks: Object.values(tasksDict) });
+  return getChartData({days, start, tasks: Object.values(tasksDict)});
 }
 
-function getChartData({ days, start, tasks = [] }) {
+// 构建 燃尽图 (echarts 的图标) 的 options
+
+function getChartData({days, start, tasks = []}) {
   if (!days || !start || !tasks) {
     return {};
   }
@@ -48,20 +53,22 @@ function getChartData({ days, start, tasks = [] }) {
 
   let max = tasks.length;
 
-  let arr = Object.keys(Array(days + 1).fill(0));
+  let tempArray = Object.keys(Array(days + 1).fill(0));
 
-  let labels = arr.map((t, index) => {
+  // x 轴刻度
+  let labels = tempArray.map((t, index) => {
     let date = new Date(start + 86400000 * t);
 
     return {
-      label: `${!index ? `${date.getFullYear()}-` : ""}${date.getMonth() +
+      label: `${!index ? `${date.getFullYear()}-` : ''}${date.getMonth() +
         1}-${date.getDate()}`,
-      date: Number(date.getTime())
+      date: Number(date.getTime()),
     };
   });
 
-  let today = Number(endOfDay(Date.now()).getTime());
+  // 实际完成任务线条数据
   let values = Array(days + 1).fill(0);
+  let today = Number(endOfDay(Date.now()).getTime());
   let count = max;
 
   for (let taskCompletedAt of tasks) {
@@ -73,58 +80,61 @@ function getChartData({ days, start, tasks = [] }) {
   }
 
   values = values.map((size, index) =>
-    today >= labels[index].date ? (count -= size) : undefined
+    today >= labels[index].date ? (count -= size) : undefined,
   );
 
   let part = max / days;
 
-  let target = arr.map(t => max - part * t);
+  // 目标数据
+  let targetData = tempArray.map(t => max - Math.round(part * t));
 
   return {
     dataSet: [
       {
-        name: "burndown",
+        // name 与 应用定义中的 reports > elements > dataName 对应
+        name: 'burndown',
+        // 图表类型的 element，data 就是 echarts 的 options
         data: {
           tooltip: {
-            trigger: "item",
-            formatter: "{a} <br/>{b}日 : 剩余{c}任务"
+            trigger: 'item',
+            formatter: '{a} <br/>{b}日 : 剩余{c}任务',
           },
           xAxis: {
-            name: "日期",
-            type: "category",
-            data: labels.map(({ label }) => label)
+            name: '日期',
+            type: 'category',
+            data: labels.map(({label}) => label),
           },
           grid: {
-            left: "4%",
-            right: "10%",
-            bottom: "2%",
-            containLabel: true
+            left: '4%',
+            right: '10%',
+            bottom: '2%',
+            containLabel: true,
           },
           yAxis: {
-            type: "value",
-            name: "任务数量",
-            max
+            type: 'value',
+            name: '任务数量',
+            max,
           },
           series: [
             {
-              name: "目标进度",
-              type: "line",
-              data: target
+              name: '目标进度',
+              type: 'line',
+              data: targetData,
             },
             {
-              name: "实际进度",
-              type: "line",
-              data: values
-            }
-          ]
-        }
-      }
-    ]
+              name: '实际进度',
+              type: 'line',
+              data: values,
+            },
+          ],
+        },
+      },
+    ],
   };
 }
 
 // start
 
-app.koa({
-  port: 9003
+app.serve({
+  port: 9003,
 });
